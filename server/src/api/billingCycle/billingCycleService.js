d652ada84c76ab4df084041ad3067a07c13edd7f
Anderson Job Loeffler <aloeffler.j@gmail.com>
2021-05-25 07:50:31 -0300

    creating API REST

---
+++ server/src/api/billingCycle/billingCycleService.js
@@ -0,0 +1,16 @@
+const BillingCycle = require('./billingCycle')
+
+BillingCycle.methods(['get', 'post', 'put', 'delete'])
+BillingCycle.updateOptions({ new: true, runValidators: true }) //Volta o objeto novo após alteração
+
+BillingCycle.route('get', (req, res, next) => {
+    BillingCycle.find({}, (err, docs) => {
+        if (!err) {
+            res.json(docs)
+        } else {
+            res.status(500).json({ errors: [error] })
+        }
+    })
+})
+
+module.exports = BillingCycle
