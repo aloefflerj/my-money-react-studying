005e2474e0cc4500507b6c44fd35cde0bb931e1a
Anderson Job Loeffler <aloeffler.j@gmail.com>
2021-05-25 07:15:37 -0300

    First commit & server configurations

---
+++ server/src/config/database.js
@@ -0,0 +1,12 @@
+const CONNECTION_URL = require('./connectionUrl')
+
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+
+mongoose.set('useFindAndModify', false)
+
+module.exports = mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+//7qZy3bwSlcRTtdry
