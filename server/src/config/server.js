af65fec9eeeb2691a578c7ae0682aeb9fc72f66e
Anderson Job Loeffler <aloeffler.j@gmail.com>
2021-05-29 18:09:01 -0300

    Middleware query parser int and backend closed

--- server/src/config/server.js
+++ server/src/config/server.js
@@ -3,10 +3,12 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const server = express()
 const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(queryParser())
 
 server.listen(port, function () {
     console.log(`Backend is running on port ${port}`)
