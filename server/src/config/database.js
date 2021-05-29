35f25f5e3917231834ba0517c643bf5fbad86925
Anderson Job Loeffler <aloeffler.j@gmail.com>
2021-05-29 16:47:56 -0300

    Error handler from api set

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -12,8 +12,8 @@ module.exports = mongoose.connect(CONNECTION_URL, {
 
 mongoose.Error.messages.general.required = 'O atributo {PATH} é obrigatório.'
 mongoose.Error.messages.Number.min =
-    'O {VALUE} informado é menor que o limite mínimo de "{MIN}".'
+    "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
 mongoose.Error.messages.Number.max =
-    'O {VALUE} informado é maior que o limite máximo de "{MAX}".'
+    "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
 mongoose.Error.messages.String.enum =
-    '"{VALUE}" não é válido para o atributo "{PATH}".'
+    "'{VALUE}' não é válido para o atributo '{PATH}'."
