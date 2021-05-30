6c6dbb303345c24bd60fa400ad26e11f2087c208
Anderson Job Loeffler <aloeffler.j@gmail.com>
2021-05-29 22:31:16 -0300

    Footer component

---
+++ client/src/common/template/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2021 
+            <a href='https://agenciakapi.com' target='_blank'>
+                Agência Kapi
+            </a>
+        </strong>
+    </footer>
+)
