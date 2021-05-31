fb63a45d383c7354bbce0c7f9a1676cd5bb3823e
Anderson Job Loeffler <aloeffler.j@gmail.com>
2021-05-31 16:08:56 -0300

    Using Valuebox compontent on dashboard component

--- client/src/dashboard/dashboard.jsx
+++ client/src/dashboard/dashboard.jsx
@@ -2,13 +2,39 @@ import React, { Component } from 'react'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+import Valuebox from '../common/widgets/valueBox'
+import Row from '../common/layout/row'
 
 class Dashboard extends Component {
     render() {
         return (
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0' />
-                <Content>Dashboard</Content>
+                <Content>
+                    <Row>
+                        <Valuebox
+                            cols='12 4'
+                            color='green'
+                            icon='bank'
+                            value='R$ 10'
+                            text='Total de Créditos'
+                        />
+                        <Valuebox
+                            cols='12 4'
+                            color='red'
+                            icon='credit-card'
+                            value='R$ 10'
+                            text='Total de Débitos'
+                        />
+                        <Valuebox
+                            cols='12 4'
+                            color='blue'
+                            icon='money'
+                            value='R$ 0'
+                            text='Valor Consolidade'
+                        />
+                    </Row>
+                </Content>
             </div>
         )
     }
